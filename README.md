ea7d0b1a1c315c1a7353aec7c87a2e369e00fcf4
douchaoyang <douchaoyang@gmail.com>
2017-12-25 18:30:19 +0800

    update eventdispatcher

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # EventDispatcher
+JavaScript事件派发器
+
+特点：`单例模式`
+
+* `使用方法`：
+
+```javascript
+Signal.on(type,caller,listener); //监听事件
+Signal.off(type,caller,listener);//移除事件
+Signal.event(type,data);         //派发事件
+Signal.offAll(type);             //移除某一事件类型的所以监听
+```
+
+* [`关于作者`](http://www.douchaoyang.com)
---
+++ plug.EventDispatcher.js
@@ -0,0 +1,52 @@
+﻿/*
+**@Author  douchaoyang
+*/
+window.Signal = window.Signal || (function() {
+	function EventDispatcher() {
+		// 已监听的事件组
+		this._events = {};
+	};
+	EventDispatcher.prototype.event = function(type, data = null) {
+		if (!this._events || !this._events[type]) 
+			return;
+		var listeners = this._events[type];
+		for (var i = 0, n = listeners.length; i < n; i++) {
+			var item = listeners[i];
+			var handler = item.handler;
+			var context = item.context;
+			handler.apply(context, [data]);
+		}
+	};
+	EventDispatcher.prototype.on = function(type, context, handler) {
+		var listeners = this._events[type];
+		if (listeners === undefined) {
+			listeners = [];
+			this._events[type] = listeners;
+		}
+		var item = {
+			handler: handler,
+			context: context
+		};
+		listeners.push(item);
+		return item;
+	};
+	EventDispatcher.prototype.off = function(type, context, handler) {
+		var listeners = this._events[type];
+		if (listeners !== undefined) {
+			for (var i = 0, n = listeners.length; i < n; i++) {
+				var item = listeners[i];
+				if (item.handler === handler && item.context === context) {
+					listeners.splice(i, 1);
+					return;
+				}
+			}
+		}
+	};
+	EventDispatcher.prototype.offAll = function(type) {
+		if (!this._events || !this._events[type]) 
+			return;
+		delete this._events[type];
+
+	};
+	return new EventDispatcher()
+})();
