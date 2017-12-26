836bf03ee6723381e38eb4bddd47172725050f6d
douchaoyang <douchaoyang@gmail.com>
2017-12-26 10:33:53 +0800

    update instance

--- listener.js
+++ listener.js
@@ -3,6 +3,10 @@ Signal.on(usernameEvent, document.getElementById("copyusername"),function(data){
 	document.getElementById("copyusername").innerHTML=data?data:"copy username...";
 });
 
-Signal.on(usernameEvent, document.getElementById("reverseusername"),function(data){
+function onreverse(data){
 	document.getElementById("reverseusername").innerHTML=data?data.split('').reverse().join(''):"reverse username...";
-});
+}
+
+Signal.on(usernameEvent, document.getElementById("reverseusername"),onreverse);
+
+// Signal.off(usernameEvent, document.getElementById("reverseusername"),onreverse);
